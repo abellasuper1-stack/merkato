ab192d6cf77c417de882f078efc70d3fac1a607a
abellasuper1 <abellasuper1@gmail.com>
2026-06-10 12:34:37 +0300

    i have added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Header from './components/header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Header/>
   <h1 className="bg-green-400"> this is going to GIThub </h1>    
     </>
   )
