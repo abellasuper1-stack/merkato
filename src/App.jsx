8b30719a1e3ef8af897fbab8504f169109a8107e
Abiabisho <abiabisho34@gmail.com>
2026-06-19 02:20:43 -0700

    fix issue

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,18 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import Header from './components/header'
 import Footer from './components/Footer'
+import Home from './page/home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Header/>
-  <h1 className="bg-green-400"> this is going to GIThub </h1>    
-  <h1 className="bg-green-400 text-white"> giithub </h1>  
-  <input type="text" className="border-2 border-gray-300 rounded-md p-2" placeholder="Enter text here" />  
-    <Footer/>
+      <Header />
+      <h1 className="bg-green-400"> this is going to GIThub </h1>
+      <h1 className="bg-green-400 text-white"> giithub </h1>
+      <input type="text" className="border-2 border-gray-300 rounded-md p-2" placeholder="Enter text here" />
+      <Footer />
     </>
   )
 }
